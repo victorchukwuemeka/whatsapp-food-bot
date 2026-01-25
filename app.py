6d0d51af817d4f508ee1e4c4b3ca70b5e52138cb
viktr <chukwuemekavictor693@gmail.com>
2026-01-25 14:58:44 +0100

    added hub to challenge

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 def verify():
     mode = request.args.get("hub.mode")
     token = request.args.get("hub.verify_token")
-    challenge = request.args.get("challenge")
+    challenge = request.args.get("hub.challenge")
 
     if mode ==  "subscribe" and token == VERIFY_TOKEN :
         return challenge, 200 
